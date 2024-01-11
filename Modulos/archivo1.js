12ec1da49ba7b5c6ab73c74949d9f878ee6eeaff
diegodfp.fly <diegodfp.fly@gmail.com>
2024-01-10 21:32:26 -0500

    update

---
+++ Modulos/archivo1.js
@@ -0,0 +1,7 @@
+// exportar una funcion 
+export function suma (a,b) {
+    return a + b;
+}
+
+// exportar una constante 
+export const pi = 3.1416;
---
+++ Modulos/exportDefaultmensaje.js
@@ -0,0 +1,7 @@
+const mensaje = () =>{
+    const nombre = "Diego";
+    const edad =  27;
+    return `${nombre} tiene ${edad} años`;   
+}
+
+export default mensaje;
